55313752984e0ce65225ec38f6582ba08eeccd20
Daniil Aznabaev <dan.aznob@gmail.com>
2024-04-24 19:56:52 +0300

    update ui.py

--- seminar_8/ui.py
+++ seminar_8/ui.py
@@ -1,11 +1,11 @@
 from logger import input_data, print_data, change_data, delete_data
 
 def interface():
-    print("Добрый день! Вы попали на специальный бот справочник от GeekBrains! \n 1 - запись данных \n 2 - вывод данных \n 3 - изменение данных \n 4 - удаление данных")
-    command = int(input('Введите число: '))
+    print("Добрый день! Вы попали на специальный бот справочник от GeekBrains! \n   Доступные команды: \n   1 - запись данных \n   2 - вывод данных \n   3 - изменение данных \n   4 - удаление данных")
+    command = int(input('Введите номер команды: '))
     while command != 1 and command != 2 and command != 3 and command != 4:
-        print('Неправильный ввод')
-        command = int(input('Введите число: '))
+        print('Неправильный ввод!')
+        command = int(input('Введите номер команды: '))
     if command == 1:
         input_data()
     elif command == 2:
